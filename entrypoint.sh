7b2202aadf41c702ce094107afb766250a4c8482
Philippe Laroche <philippe.laroche@agoralogie.fr>
2026-06-05 16:49:33 +0200

    messagerie

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,6 @@ fi
 chown -R www-data:www-data /var/www/data 2>/dev/null || true
 chown -R www-data:www-data /var/www/html/media 2>/dev/null || true
 mkdir -p /var/www/html/uploads/produits
+mkdir -p /var/www/html/uploads/messages
 chown -R www-data:www-data /var/www/html/uploads 2>/dev/null || true
 exec "$@"
